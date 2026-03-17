d21587b920e28b68038332653a6293ed2fa33798
OrkVili <n7n1mg@uni-sopron.hu>
2026-03-17 18:19:55 +0100

    Update models

--- car_care/service_app/admin.py
+++ car_care/service_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import Vehicle, Part, Service, ServicePart
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(Vehicle)
+admin.site.register(Part)
+admin.site.register(Service)
+admin.site.register(ServicePart)
--- car_care/service_app/models.py
+++ car_care/service_app/models.py
@@ -1,10 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=24)
-    password = models.CharField(max_length=24)
-    role = models.BooleanField(default=False)
-
 class Vehicle(models.Model):
-    pass
+    make = models.CharField(max_length=64)
+    model = models.CharField(max_length=64)
+    license_plate = models.CharField(max_length=24, null=True, blank=True)
+    year = models.IntegerField()
+    fuel = models.CharField(max_length=24, null=True, blank=True)
+    purchase_price = models.IntegerField(default=0)
+    purchase_odometer = models.IntegerField(default=0)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.make} {self.model} ({self.year}) - {self.owner.username}"
+
+class Service(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    odometer = models.IntegerField(null=True, blank=True)
+    time = models.DateTimeField(default=timezone.now)
+    labor_cost = models.IntegerField(default=0)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}({self.vehicle.model}) - {self.labor_cost} | {self.time}"
+
+class Part(models.Model):
+    name = models.CharField(max_length=255)
+    article_number = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} - {self.quantity}pc, {self.price}/quantity"
+
+class ServicePart(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    quantity_used = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.service.title} - {self.part.name} ({self.quantity_used}pc)"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if self.quantity_used > self.part.quantity:
+                self.quantity_used = self.part.quantity
+                self.part.quantity = 0
+            else:
+                self.part.quantity -= self.quantity_used
+                self.part.save()
+        super().save(*args, **kwargs)
+
